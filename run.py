a7049726e95c4ba0f6b936e2aacbc5e28a52bc88
Dana Gertsch <dgertsch@kovarus.com>
2019-01-23 11:19:17 -0700

    First commit

---
+++ run.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_restplus import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'some-secret-string'
+
+db = SQLAlchemy(app)
+
+jwt = JWTManager(app)
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+import routes, models
+
+if __name__ == '__main__':
+    app.run(debug=True)
